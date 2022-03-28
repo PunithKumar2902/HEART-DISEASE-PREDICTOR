d4f093ba85f4c4be6c7f0948bc0aed5160d1341b
Punith Kumar <66864406+PunithKumar2902@users.noreply.github.com>
2022-03-28 15:31:16 +0530

    Create heart disease predictor.py

---
+++ heart disease predictor.py
@@ -0,0 +1,135 @@
+# importing the required libraries
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+%matplotlib inline
+
+#visualizing the data
+
+df = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/heart.csv")
+df.head()
+
+df.describe()
+
+df['target'].value_counts().plot(kind="bar", color=["red", "lightblue"])
+
+df.isnull().sum()
+
+categorical_val = []
+continous_val = []
+for column in df.columns:
+    print('==============================')
+    print(f"{column} : {df[column].unique()}")
+    if len(df[column].unique()) <= 10:
+        categorical_val.append(column)
+    else:
+        continous_val.append(column)
+        
+#EXPLORATORY DATA ANALYSIS
+
+plt.figure(figsize=(15, 15))
+
+for i, column in enumerate(categorical_val, 1):
+    plt.subplot(3, 3, i)
+    df[df["target"] == 0][column].hist(bins=35, color='blue', label='Have Heart Disease = NO', alpha=0.6)
+    df[df["target"] == 1][column].hist(bins=35, color='red', label='Have Heart Disease = YES', alpha=0.6)
+    plt.legend()
+    plt.xlabel(column)
+    
+
+plt.figure(figsize=(15, 15))
+
+for i, column in enumerate(continous_val, 1):
+    plt.subplot(3, 2, i)
+    df[df["target"] == 0][column].hist(bins=35, color='blue', label='Have Heart Disease = NO', alpha=0.6)
+    df[df["target"] == 1][column].hist(bins=35, color='red', label='Have Heart Disease = YES', alpha=0.6)
+    plt.legend()
+    plt.xlabel(column)
+    
+# Create another figure
+plt.figure(figsize=(10, 8))
+
+# Scatter with postivie examples
+plt.scatter(df.age[df.target==1],
+            df.thalach[df.target==1],
+            c="salmon")
+
+# Scatter with negative examples
+plt.scatter(df.age[df.target==0],
+            df.thalach[df.target==0],
+            c="lightblue")
+
+# Add some helpful info
+plt.title("Heart Disease in function of Age and Max Heart Rate")
+plt.xlabel("Age")
+plt.ylabel("Max Heart Rate")
+plt.legend(["Disease", "No Disease"]);
+
+# Let's make our correlation matrix
+corr_matrix = df.corr()
+fig, ax = plt.subplots(figsize=(15, 15))
+ax = sns.heatmap(corr_matrix,
+                 annot=True);
+
+# lets see correlation with target
+df.drop('target', axis=1).corrwith(df.target).plot(kind='bar', grid=True, figsize=(12, 8), 
+                                                   title="Correlation with target")
+
+#DATA PROCESSING
+categorical_val.remove('target')
+dataset = pd.get_dummies(df, columns = categorical_val)
+
+from sklearn.preprocessing import StandardScaler
+
+s_sc = StandardScaler()
+col_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+dataset[col_to_scale] = s_sc.fit_transform(dataset[col_to_scale])
+
+
+#Splitting the data set
+from sklearn.model_selection import train_test_split
+
+X = dataset.drop('target', axis=1)
+y = dataset.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+#Building the Logistic Regression model
+from sklearn.linear_model import LogisticRegression
+lr_clf = LogisticRegression()
+lr_clf.fit(X_train, y_train)
+
+
+#Evaluating the model
+
+#1.On Training Data
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+pred = lr_clf.predict(X_train)
+clf_report = pd.DataFrame(classification_report(y_train, pred, output_dict=True))
+print("Train Result:\n================================================")
+print(f"Accuracy Score: {accuracy_score(y_train, pred) * 100:.2f}%")
+print("_______________________________________________")
+print(f"CLASSIFICATION REPORT:\n{clf_report}")
+print("_______________________________________________")
+print(f"Confusion Matrix: \n {confusion_matrix(y_train, pred)}\n")
+
+#2.On Test Data
+pred = lr_clf.predict(X_test)
+clf_report = pd.DataFrame(classification_report(y_test, pred, output_dict=True))
+print("Test Result:\n================================================")        
+print(f"Accuracy Score: {accuracy_score(y_test, pred) * 100:.2f}%")
+print("_______________________________________________")
+print(f"CLASSIFICATION REPORT:\n{clf_report}")
+print("_______________________________________________")
+print(f"Confusion Matrix: \n {confusion_matrix(y_test, pred)}\n")
+
+test_score = accuracy_score(y_test, lr_clf.predict(X_test)) * 100
+train_score = accuracy_score(y_train, lr_clf.predict(X_train)) * 100
+
+results_df = pd.DataFrame(data=[["Logistic Regression", train_score, test_score]], 
+                          columns=['Model', 'Training Accuracy %', 'Testing Accuracy %'])
+print(results_df)
